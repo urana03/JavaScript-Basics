7cdc56cb83d9e3fa9a01cf7ac1f49f8b8d2285d9
urana03 <uhackninja@gmail.com>
2023-01-06 11:17:27 +0530

    Add: Javascript file for showing javascript example

---
+++ Intro-to-js/example.js
@@ -0,0 +1,11 @@
+document.write("Javascript is a simple language for learning");
+function msg(){
+    alert("Welcome to the Javascript World")
+}
+
+function sum(){
+    var a = 15;
+    var b = 16;
+    var x = a+b;
+    document.write(x);
+}
