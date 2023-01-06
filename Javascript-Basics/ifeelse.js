5fb53b13ea9835348517d1fe46a9371afe1529bd
urana03 <uhackninja@gmail.com>
2023-01-06 11:32:57 +0530

    Add: To show various if and if else statement

---
+++ Javascript-Basics/ifeelse.js
@@ -0,0 +1,29 @@
+// if statement
+var a = 11;
+if(a >10){
+    document.write("The number is greater than 10 <br/> ");
+}
+
+//if-else statement
+var a = 21;
+if(a%2==0){
+   document.write(" The number is even <br/>");
+}
+else{
+   document.write(" The number is even <br/>")
+}
+
+//if else-if statement
+var a = 15;
+if(a == 10){
+       document.write("The number is 10 ");
+}
+else if(a == 15){
+      document.write("The number is 15 <br/>");
+}
+else if(a == 20){
+      document.write("The number is 20");
+}
+else{
+     document.write("The number is 10,15 or 20")
+}
