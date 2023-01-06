544aba1d40c9ab1d369abdecebd57d832d580807
urana03 <uhackninja@gmail.com>
2023-01-06 11:34:56 +0530

    Add: To show the switch cases and the working of it

---
+++ Javascript-Basics/switchcase.js
@@ -0,0 +1,18 @@
+var grade = 'B';
+var result;
+
+switch(grade){
+    case 'A':
+        result='Grade A';
+    break;
+    case 'B':
+        result = 'Grade B <br/> ';
+    break;
+    case 'C':
+        result = 'Grade C';
+    break;
+    case 'D':
+        result = 'Grade D';
+    break;
+}
+document.write(result);
