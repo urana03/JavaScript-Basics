935b7bf2af08a354a3a109e6929ee21c0be11764
urana03 <uhackninja@gmail.com>
2023-01-06 11:34:13 +0530

    Add: To show the various loops and their working

---
+++ Javascript-Basics/loops.js
@@ -0,0 +1,22 @@
+// for loop
+for(let i = 0; i<=5; i++){
+    document.write(i + " ");
+}
+document.write("<br/>");
+
+// while loop
+var a = 11;
+while(a<=15)
+{
+    document.write(a+" ");
+    a++;
+}
+document.write("<br/>");
+
+// do while loop
+var a = 22;
+do{
+    document.write(a+" ");
+}
+while(a == 20);
+document.write("<br/>");
